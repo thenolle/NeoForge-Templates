aa4f243c2a8a16255ac3997d66c5e7a99ade90b2
TheNolle <github@thenolle.com>
2026-07-05 22:30:12 +0200

    feat: added sqlite to server

--- src/main/kotlin/com/nolly/mc/template_neoforge/TemplateNeoForge.kt
+++ src/main/kotlin/com/nolly/mc/template_neoforge/TemplateNeoForge.kt
@@ -2,16 +2,13 @@ package com.nolly.mc.template_neoforge
 
 import com.nolly.mc.template_neoforge.database.Database
 import com.nolly.mc.template_neoforge.database.PlayerStatsRepository
-import net.neoforged.api.distmarker.Dist
 import net.neoforged.fml.common.Mod
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent
 import net.neoforged.fml.event.lifecycle.FMLDedicatedServerSetupEvent
 import net.neoforged.fml.loading.FMLPaths
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 import thedarkcolour.kotlinforforge.neoforge.forge.MOD_BUS
-import thedarkcolour.kotlinforforge.neoforge.forge.runWhenOn
 
 @Mod(TemplateNeoForge.ID)
 object TemplateNeoForge {
@@ -24,26 +21,17 @@ object TemplateNeoForge {
 	init {
 		LOGGER.info("Initializing $ID")
 		MOD_BUS.addListener(::onCommonSetup)
-		runWhenOn(Dist.CLIENT) {
-			MOD_BUS.addListener(::onClientSetup)
-		}
-		runWhenOn(Dist.DEDICATED_SERVER) {
-			MOD_BUS.addListener(::onServerSetup)
-		}
+		MOD_BUS.addListener(::onServerSetup)
 	}
 
 	private fun onCommonSetup(event: FMLCommonSetupEvent) {
+		LOGGER.info("Common setup complete")
+	}
+
+	private fun onServerSetup(event: FMLDedicatedServerSetupEvent) {
 		val databasePath = FMLPaths.CONFIGDIR.get().resolve("$ID/data.db")
 		database = Database(databasePath)
 		playerStats = PlayerStatsRepository(database).also { it.createTable() }
 		LOGGER.info("SQLite database ready at $databasePath")
-	}
-
-	private fun onClientSetup(event: FMLClientSetupEvent) {
-		LOGGER.info("Client setup complete")
-	}
-
-	private fun onServerSetup(event: FMLDedicatedServerSetupEvent) {
-		LOGGER.info("Dedicated server setup complete")
 	}
 }
